2bcf70b0f0bd617e43739d01867adb16c7060eb6
Ziang Wan <zawtom666@gmail.com>
2017-10-25 11:53:19 -0500

    file transmit online:

--- client.c
+++ client.c
@@ -35,13 +35,13 @@ int main(){
 	// get the file and its size
 	FILE* f = fopen("test.txt", "r");
 	fseek(f, 0L, SEEK_END);
-	long file_size = ftell(f);
+	long file_size = ftell(f) - 1;
 	rewind(f);
 
 	// get the data ready
 	FILE_TRANSMIT* data_sent = calloc(sizeof(FILE_TRANSMIT), 0);
 	data_sent->meta_data.data_length = sizeof(META_DATA) + 32 + file_size;
-	data_sent->meta_data.func_code = FUNC_FILE_TRANSMIT;
+	data_sent->meta_data.func_code = 1;
 	strcpy(data_sent->file_name, "test.txt");
 	fread(data_sent->buffer, sizeof(char), file_size, f);
 
@@ -52,6 +52,8 @@ int main(){
 		return 0;
 	}
 
+	printf("send done\n");
+
 	// receive a confirmation
 	char* buf_recv = calloc(128, sizeof(char));
 	retCode = recv(sock, buf_recv, 128, 0);
@@ -59,6 +61,9 @@ int main(){
 		printf("recv failed\n");
 		return 0;
 	}
+	printf("%s\n", buf_recv);
+
+	printf("recv done\n");
 
 	// char* buf = malloc(128);
 	// char* buf_recv = calloc(128, sizeof(char));
