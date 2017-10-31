2573610b47f890080053f50fb86f68d3306d6bf6
Ziang Wan <zawtom666@gmail.com>
2017-10-30 22:48:47 -0500

    protocal updated

--- proto_client_master.h
+++ proto_client_master.h
@@ -3,18 +3,36 @@
 #define K 1024
 
 typedef struct meta_data_t {
+	// not including the length of meta_data
 	unsigned 	data_length;
 	int 		func_code;
 } META_DATA;
 
 // function code
-#define FUNC_FILE_TRANSMIT 	0
+#define FUNC_FILE_UPLOAD 	0
+// download
+#define FUNC_FILE_DOWNLOAD 	1
 // to be used later
-#define FUNC_EXECUTE_CMD 	1
+#define FUNC_EXECUTE_CMD 	2
+// feedback
+#define FUNC_FEEDBACK		3
 
 // file transmission protocal
-typedef struct file_trans_t {
+typedef struct file_transmit_t {
 	META_DATA 	meta_data;	// funcCode = 0
 	char		file_name[32];
 	char 		buffer[16 * K];
 } FILE_TRANSMIT;
+
+// execute basic shell command
+typedef struct execute_cmd_t {
+	META_DATA 	meta_data;
+	char 		command[1 * K];
+} EXECUTE_CMD;
+
+// feedback struct
+typedef struct feedback_t
+{
+	META_DATA 	meta_data;
+	char 		feedback[16 * K];
+} FEEDBACK;
