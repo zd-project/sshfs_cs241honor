654a263445c69f9ad79a5196a8f93301a84d53c8
Gary Liu <jl25@linux-a2.ews.illinois.edu>
2017-12-05 12:04:29 -0600

    Filemgr support slave to file list

--- filemgr.c
+++ filemgr.c
@@ -9,6 +9,8 @@ Filemgr filemgr;
 
 void Filemgr_init () {
 	memset(filemgr.f2s, FILEMGR_NOFILE, sizeof(filemgr.f2s));
+	memset(filemgr.s2f, '\0', sizeof(filemgr.s2f));
+	memset(filemgr.cnt, 0, sizeof(filemgr.f2s));
 }
 
 void Filemgr_init_slave (Slaveid slave_id, int slave_fd) {
@@ -16,7 +18,7 @@ void Filemgr_init_slave (Slaveid slave_id, int slave_fd) {
 	int file_list_len = read(slave_fd, file_list, 65536);
 	file_list[file_list_len] = '\0';
 	
-	char file_name[32];
+	char file_name[FILENAME_LEN];
 	while (sscanf(file_list, "%s", file_name) != EOF) {
 		Filemgr_set(file_name, slave_id);
 	}
@@ -25,11 +27,17 @@ void Filemgr_init_slave (Slaveid slave_id, int slave_fd) {
 void Filemgr_set (char* filename, Slaveid slave_id) {
 	Hash filehash = Filemgr_hash(filename);
 	filemgr.f2s[filehash] = slave_id;
+	memcpy(filemgr.s2f[slave_id][filemgr.cnt[slave_id]], filename, strlen(filename) + 1);
+	filemgr.cnt[slave_id] ++;
 }
 
 Slaveid Filemgr_get (char* filename) {
 	Hash filehash = Filemgr_hash(filename);
 	return filemgr.f2s[filehash];
+}
+
+char** Filemgr_get (Slaveid slave_id) {
+	return filemgr.s2f[slave_id];
 }
 
 Hash Filemgr_hash (char* filename) {
--- types.h
+++ types.h
@@ -3,6 +3,9 @@
 typedef uint8_t Slaveid;
 #define SLAVE_MAX (1<<(sizeof(Slaveid)))
 
+#define FILENAME_LEN 32
+#define FILE_PER_SLAVE 256
+
 typedef uint16_t Hash;
 #define FILEMGR_HASH_MAX (1<<(sizeof(Hash)))
 #define FILEMGR_NOFILE (FILEMGR_HASH_MAX-1)
