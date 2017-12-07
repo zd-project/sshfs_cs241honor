b6e6a947b9de4d5f8acfc1f06c13b537f0c13290
Gary Liu <jl25@linux-a2.ews.illinois.edu>
2017-12-06 19:20:22 -0600

    Protocol

--- protocol.h
+++ protocol.h
@@ -1,33 +1,18 @@
 #pragma once
 
+#include "types.h"
+
 #define K 1024
 
 typedef struct {
-	uint32_t len;  // length of subsequent message: (0,16K)
+	uint8_t func_code;
+	char file_name[FILENAME_LEN];
 	char buf[16 * K];
 } Message;
 
-typedef struct {
-	uint32_t len;
-	uint32_t func_code;
-	char buf[16 * K - sizeof(uint32_t)];
-} MessageInput;
-
-typedef struct {
-	uint32_t len;
-	uint32_t func_code;
-	char cmd_buf[16 * K - sizeof(uint32_t)];
-} MessageExecCmd;
-
-typedef struct {
-	uint32_t len;
-	uint32_t func_code;
-	char file_name[32];
-	char file_buf[16 * K - sizeof(uint32_t) - sizeof(char[32])];
-} MessageFileTransmit;
-
-#define FUNC_FILE_TRANSMIT 0
-#define FUNC_EXEC_CMD 1
+#define FUNC_EXEC 0
+#define FUNC_UPLD 1
+#define FUNC_DNLD 2
 
 typedef struct {
 	uint8_t func_code;
